79585609aff9c24e8ec621331ee6436cfb850287
Guy <113591228+guy-nicholls-cred@users.noreply.github.com>
2022-12-01 08:54:30 +0000

    Add day 1 AOC

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,15 +3,41 @@ package days
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.uppercase() }
-            .joinToString(" ")
+        var highestElf = 0
+        var currentElf = 0
+        for(input in inputList){
+            if(input != ""){
+                currentElf += Integer.parseInt(input)
+            } else {
+                if(currentElf > highestElf){
+                    highestElf = currentElf
+                }
+                currentElf = 0
+            }
+        }
+        return highestElf
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.uppercase() }
-            .last()
+        var highestElves = mutableListOf<Int>()
+        var currentElf = 0
+
+        for(input in inputList){
+            if(input != ""){
+                currentElf += Integer.parseInt(input)
+            } else {
+                if(highestElves.size < 3){
+                    highestElves.add(currentElf)
+                } else {
+                    highestElves.sort()
+                    if(currentElf > highestElves[0]){
+                        highestElves[0] = currentElf
+                    }
+                }
+                currentElf = 0
+            }
+        }
+
+        return highestElves[0] + highestElves[1] + highestElves[2]
     }
 }
